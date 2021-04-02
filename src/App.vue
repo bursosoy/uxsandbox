2fda3265673d3fa92207dfe2af3e80cbf547a7c0
Anthony Pena <bursosoy@gmail.com>
2021-04-02 18:04:00 +0800

    Added data types

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <div class="parent">
-    <div class="child">test</div>
-  </div>
+  <div>anyNumber: {{ anyNumber }}</div>
+  <div>anyString: {{ anyString }}</div>
+  <div>anyBoolean: {{ anyBoolean }}</div>
+  <div>anyArray at 1: {{ anyArray[1] }}</div>
+  <div>anyObject color: {{ anyObject.color }}</div>
+
+  <div v-if="2<1">I'm a div</div>
+  <!-- if you want to render it and just hide, use v-show. if you want to save on data loading, use v-if -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      anyNumber: 0, // Number
+      anyString: 'TestString', // String
+      anyBoolean: true, // Boolean
+      anyArray: ["red","blue","green"], // Array is a container!
+      anyObject: {color: "gray"}, // Object
+
+      isDivShown: true // Boolean
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.parent{
-  .child{
-    font-weight: bold;
-  }
+body{
+  font-family: Alte Haas Grotesk;
+  font-size: 1.5rem;
 }
 </style>
