1424905efcc4f7adc5b1eabdbeb748a0cb1cb15a
Anthony Pena <bursosoy@gmail.com>
2021-04-03 14:21:32 +0800

    Added grocery list

---
+++ src/views/Cart.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="main-wrap">
+      <div class="item">
+          <div class="quantity">{{ item1.quantity }}</div>
+          <div>{{ item1.label }}</div>
+          <div class="price">${{ item1.price }}</div>
+          <div class="plusminus">
+              <div v-if="item1.quantity > 0" @click="updateQuantity('minus',item1)">-</div>
+              <div @click="updateQuantity('plus',item1)">+</div>
+          </div>
+      </div>
+      <div class="item">
+          <div class="quantity">{{ item2.quantity }}</div>
+          <div>{{ item2.label }}</div>
+          <div class="price">${{ item2.price }}</div>
+          <div class="plusminus">
+              <div v-if="item2.quantity > 0" @click="updateQuantity('minus',item2)">-</div>
+              <div @click="updateQuantity('plus',item2)">+</div>
+          </div>
+      </div>
+      <div class="item">
+          <div class="quantity">{{ item3.quantity }}</div>
+          <div>{{ item3.label }}</div>
+          <div class="price">${{ item3.price }}</div>
+          <div class="plusminus">
+              <div v-if="item3.quantity > 0" @click="updateQuantity('minus',item3)">-</div>
+              <div @click="updateQuantity('plus',item3)">+</div>
+          </div>
+      </div>
+      <div class="total">
+          <div>Total</div>
+          <div>${{ getTotal() }}</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            item1:{
+                label: "Beef Steak",
+                isHovered: false,
+                price: 10.00,
+                quantity: 0,
+                bought: 0
+            },
+            item2:{
+                label: "Carrot",
+                isHovered: false,
+                price: 1.50,
+                quantity: 0,
+                bought: 0
+            },
+            item3:{
+                label: "French Bread",
+                isHovered: false,
+                price: 2.50,
+                quantity: 0,
+                bought: 0
+            }
+        }
+    },
+    methods:{
+        getTotal(){
+            return (this.item1.quantity * this.item1.price)+(this.item2.quantity * this.item2.price)+(this.item3.quantity * this.item3.price)
+        },
+        updateQuantity(mode,item){
+            if (mode === "minus"){
+                item.quantity--
+            }
+            else{
+                item.quantity++
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700;900&display=swap');
+.main-wrap{
+    display: inline-flex;
+    flex-direction: column;
+    font-family: Source Sans Pro;
+    font-weight: 400;
+
+    .item, .total{
+        min-width: 20rem;
+        margin: 0.5rem;
+        padding: 1rem;
+    }
+
+    .item{
+        display: inline-flex;
+        align-items: center;
+        background-color: #eee;
+        border-radius: 0.5rem;
+
+        .quantity{
+            margin-right: 1rem;
+        }
+
+        .price{
+            margin-left: auto;
+        }
+
+        .plusminus{
+            display: flex;
+
+            div{
+                background-color: #ccc;
+                padding: 0.5rem 1rem;
+                margin-left: 0.5rem;
+                border-radius: 0.25rem;
+                cursor: pointer;
+            }
+        }
+    }
+
+    .total{
+        display: flex;
+        justify-content: space-between;
+        background-color: #888;
+        color: #fff;
+        font-weight: 700;
+        border-radius: 0.5rem;
+    }
+}
+</style>
---
+++ src/views/DataTypes&Functions.vue
@@ -0,0 +1,66 @@
+<template>
+  <!-- <div>anyNumber: {{ anyNumber }}</div>
+  <div>anyString: {{ anyString }}</div>
+  <div>anyBoolean: {{ anyBoolean }}</div>
+  <div>anyArray at 1: {{ anyArray[1] }}</div>
+  <div>anyObject color: {{ anyObject.color }}</div>
+
+  <div v-if="anyNumber === 0 && anyBoolean === false">I'm a div</div> -->
+  <!-- <div>{{ checkUser("Carlo") }}</div> -->
+
+  <!-- <button @click="logName">Say Name</button>
+  <button @click="logNewName('Kiyu')">Say New Name</button> -->
+  <!-- <button @click="logNewName(checkUser('Carlo'))">Check User</button> -->
+  <div v-if="checkUser('Kiyu') && anyNumber === 0">Show me if its Kiyu!</div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      anyNumber: 0, // Number
+      anyString: 'TestString', // String
+      anyBoolean: true, // Boolean
+      anyArray: ["red","blue","green"], // Array is a container!
+      anyObject: {color: "gray"}, // Object
+
+      isDivShown: true // Boolean
+    }
+  },
+  methods:{
+    logName(){ // basic type -> performs tasks
+      console.log("Anthony")
+      console.log("Dog")
+      var myNumber = 1+1
+      myNumber = 8
+      console.log(myNumber)
+    },
+
+    logNewName(newName){ // accepts arguments and passes it to its parameter
+      console.log(newName + " Tran")
+    },
+
+    logAge(additionalAge){ // accepts argument and returns something
+      return additionalAge * 2
+    },
+
+    checkUser(name){ // function that returns based on a condition
+      if(name === "Kiyu"){
+        return true
+      }
+      else{
+        return false
+      }
+    }
+
+  }
+}
+</script>
+
+<style lang="scss">
+body{
+  font-family: Alte Haas Grotesk;
+  font-size: 1.5rem;
+}
+</style>
